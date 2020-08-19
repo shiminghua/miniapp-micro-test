6b714e1c74a6336ebb6ea307c52e996f822e1c87
shiminghua <shiminghua2008@qq.com>
2020-08-19 15:06:58 +0800

    a

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,10 +10,21 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+  // 事件
+  gotoSubmodule1: function () {
+    wx.navigateTo({
+      url: '/miniapp-micro-submodule1/pages/index/index',
+    });
+  },
+  gotoTaro: function() {
+    wx.navigateTo({
+      url: '/miniapp-micro-taro/dist/pages/index/index',
+    });
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -21,7 +32,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,7 +54,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
